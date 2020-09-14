902d932d8e2ed8433978587e753725330068a43f
mlisareyes <mlisareyes@gmail.com>
2020-09-13 20:49:48 -0400

    added sessions controller and user resource

---
+++ recognition-api/app/controllers/users_controller.rb
@@ -0,0 +1,4 @@
+class UsersController < ApplicationController
+  def create #signup
+  end
+end
--- recognition-api/config/application.rb
+++ recognition-api/config/application.rb
@@ -23,6 +23,7 @@ module RecognitionApi
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
+    config.middleware.use ActionDispatch::Cookies
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
@@ -33,5 +34,6 @@ module RecognitionApi
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
   end
 end
