3f9c05732dd90614d2b0841193f99b64aa9f5b3f
mlisareyes <mlisareyes@gmail.com>
2020-09-17 00:02:51 -0400

    created signup component

---
+++ recognition-client/src/actions/createUser.js
@@ -0,0 +1,15 @@
+export const createUser = (payload) => {
+  return (dispatch) => {
+    fetch("http://localhost:3000/users", {
+      method: "POST",
+      credentials: "include",
+      headers: {
+        "Accept": "application/json",
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({user: payload})
+    })
+  }
+  .then(response => response.json())
+  .then(data => dispatch({type: 'CREATE_USER', user: data}))
+}
---
+++ recognition-client/src/components/SignUp.js
@@ -0,0 +1,51 @@
+import React, {Component} from 'react';
+
+class SignUp extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      first_name: "",
+      last_name: "",
+      email: "",
+      password: ""
+    }
+  }
+
+  handleOnChange = event => {
+    this.setState({
+      [event.target.name]: event.target.value
+    })
+
+  }
+
+  handleOnSubmit = event => {
+    event.preventDefault()
+    this.props.createUser({
+      first_name: this.state.first_name,
+      last_name: this.state.last_name,
+      email: this.state.email,
+      password: this.state.password
+    })
+  }
+
+  render() {
+    <div className="SignUp">
+      <form onSubmit={this.handleOnSubmit}>
+      <br />
+      <input type="text" name="firstname" placeholder="First Name" value={this.state.first_name} onChange={this.handleOnChange} />
+      <br />
+      <input type="text" name="lastname" placeholder="Last Name" value={this.state.last_name} onChange={this.handleOnChange} />
+      <br />
+      <input type="email" name="email" placeholder="email" value={this.state.email} onChange={this.handleOnChange} />
+      <br />
+      <input type="password" name="password" placeholder="password" value={this.state.password} onChange={this.handleOnChange} />
+      <br />
+      <input type="submit" value="Sign Up" />
+      </form>
+    </div>
+
+  }
+}
+
+export default SignUp
