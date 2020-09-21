a8716b4b2f9e37379c323f085e5c7457ac76f8bf
mlisareyes <mlisareyes@gmail.com>
2020-09-21 15:17:20 -0400

    fixed model associations

--- recognition-api/app/models/user.rb
+++ recognition-api/app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ApplicationRecord
   has_secure_password
+  has_many :sent_recognitions, class_name: "Recognition", foreign_key: "author_id"
+  has_many :received_recognitions, class_name: "Recognition", foreign_key: "receiver_id"
 
-  # has_many :recognitions
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :email, presence: true
