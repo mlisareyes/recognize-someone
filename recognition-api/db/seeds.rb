801cfa1201364146cc7850e63ccd1541d5637df8
mlisareyes <mlisareyes@gmail.com>
2020-09-21 16:26:28 -0400

    created user seeds

--- recognition-api/db/seeds.rb
+++ recognition-api/db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create([
+  {first_name: 'John', last_name: 'Dunwald', email: 'john.dunwald@company.com', password: 'abc123'},
+  {first_name: 'Karen', last_name: 'Foley', email: 'karen.foley@company.com', password: 'def456'},
+  {first_name: 'Richard', last_name: 'Rodriguez', email: 'richard.rodriguez@company.com', password: 'ghi789'},
+  {first_name: 'Theresa', last_name: 'Jacobs', email: 'theresa.jacobs@company.com', password: 'jkl111'}
+  ])
