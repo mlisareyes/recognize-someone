8355ad4056ac5dff1525a0e1abd8024aeaca8fc2
mlisareyes <mlisareyes@gmail.com>
2020-09-21 16:02:20 -0400

    added index and create methods

--- recognition-api/app/controllers/recognitions_controller.rb
+++ recognition-api/app/controllers/recognitions_controller.rb
@@ -1,9 +1,19 @@
 class RecognitionsController < ApplicationController
 
   def index
+    @recognitions = Recognition.all
+
+    render json: RecognitionSerializer.new(@recognition).serialized_json
   end
 
   def create
+    @recognition = Recognition.new(recognition_params)
+
+    if @recognition.save
+      render json: RecognitionSerializer.new(@recognitions).serialized_json
+    else
+      render json: {error: "Recognition not created"}
+    end
   end
 
   def show
--- recognition-api/app/controllers/users_controller.rb
+++ recognition-api/app/controllers/users_controller.rb
@@ -3,7 +3,7 @@ class UsersController < ApplicationController
 
   def index
     @users = User.all
-    ender json: UserSerializer.new(@users).serialized_json
+    render json: UserSerializer.new(@users).serialized_json
   end
 
   def create #signup
